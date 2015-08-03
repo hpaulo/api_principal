dd92ec3a3ab547f4208c236f70f31e7dbbbbbd45
Elton Nunes <elton.nunes@atoscapital.com.br>
2015-08-03 15:08:07 -0300

    Testes de UpLoad arquivo ofx

---
+++ api/Controllers/card/TesteUploadController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace api.Controllers.Card
+{
+    public class TesteUploadController : ApiController
+    {
+        // GET: api/TesteUpload
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TesteUpload/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TesteUpload
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+
+        // PUT: api/TesteUpload/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TesteUpload/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
