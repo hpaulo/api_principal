b94fbd0fb8e7aee99e831fe51dfe23f3cf39954a
Elton Nunes <elton.nunes@atoscapital.com.br>
2015-08-07 17:52:36 -0300

    Alterações em Devices

--- api/Controllers/Login/StatusController.cs
+++ api/Controllers/Login/StatusController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using api.Bibliotecas;
 
 namespace api.Controllers.Login
 {
@@ -12,7 +13,9 @@ namespace api.Controllers.Login
         // GET: /Status/
         public HttpResponseMessage Get()
         {
-            return Request.CreateResponse(HttpStatusCode.OK);
+            return Request.CreateResponse<object>(HttpStatusCode.OK, Device.Info());
         }
+
+
     }
 }
