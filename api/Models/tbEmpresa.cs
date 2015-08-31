ebb548f7abb8d8cae31c32fb2173dcddd2805098
unknown <fernando@atoscapital.com.br>
2015-08-30 22:53:38 -0300

    Leitura do certificado digital do DB

--- api/Models/tbEmpresa.cs
+++ api/Models/tbEmpresa.cs
@@ -6,7 +6,7 @@ namespace api.Models
     public partial class tbEmpresa
     {
         public string nrCNPJBase { get; set; }
-        public string dsCertificadoDigital { get; set; }
+        public byte[] dsCertificadoDigital { get; set; }
         public string dsCertificadoDigitalSenha { get; set; }
         public int cdEmpresaGrupo { get; set; }
         public virtual tbEmpresaGrupo tbEmpresaGrupo { get; set; }
